c55dc6ab82301617af1a8e89576e081fb4caba23
Cenk Gün <cenk.gn@gmail.com>
2015-05-07 14:26:34 +0300

    Tüm Şehirler

--- bot.php
+++ bot.php
@@ -4,20 +4,39 @@
 include "pdo.php";
 include "Connection.php";
 
+$iller = array('Adana', 'Adıyaman', 'Afyon', 'Ağrı', 'Amasya', 'Ankara', 'Antalya', 'Artvin',
+	'Aydın', 'Balıkesir', 'Bilecik', 'Bingöl', 'Bitlis', 'Bolu', 'Burdur', 'Bursa', 'Çanakkale',
+	'Çankırı', 'Çorum', 'Denizli', 'Diyarbakır', 'Edirne', 'Elazığ', 'Erzincan', 'Erzurum', 'Eskişehir',
+	'Gaziantep', 'Giresun', 'Gümüşhane', 'Hakkari', 'Hatay', 'Isparta', 'Mersin', 'İstanbul', 'İzmir',
+	'Kars', 'Kastamonu', 'Kayseri', 'Kırklareli', 'Kırşehir', 'Kocaeli', 'Konya', 'Kütahya', 'Malatya',
+	'Manisa', 'Kahramanmaraş', 'Mardin', 'Muğla', 'Muş', 'Nevşehir', 'Niğde', 'Ordu', 'Rize', 'Sakarya',
+	'Samsun', 'Siirt', 'Sinop', 'Sivas', 'Tekirdağ', 'Tokat', 'Trabzon', 'Tunceli', 'Şanlıurfa', 'Uşak',
+	'Van', 'Yozgat', 'Zonguldak', 'Aksaray', 'Bayburt', 'Karaman', 'Kırıkkale', 'Batman', 'Şırnak',
+	'Bartın', 'Ardahan', 'Iğdır', 'Yalova', 'Karabük', 'Kilis', 'Osmaniye', 'Düzce');
 
-$il = "samsun";
+foreach($iller as $il) {
 
-$veri_al= file_get_contents('http://www.havadurumu.com.tr/havadurumu/' .$il. '');
+	$veri_al= file_get_contents('http://www.havadurumu.com.tr/havadurumu/' .$il. '');
 
-preg_match_all('@<span class="cur_temp">\s+(.*)</span>@iu', $veri_al,$veri_derece);
+	preg_match_all('@<span class="cur_temp">\s+(.*)</span>@iu', $veri_al,$veri_derece);
 
-preg_match_all('@<img src="/icons/(.*?)" alt="(.*?)" title="(.*?)">@si',$veri_al, $veri_durum);
-
-
+	preg_match_all('@<img src="/icons/(.*?)" alt="(.*?)" title="(.*?)">@si',$veri_al, $veri_durum);
 
 
-$derece= $veri_derece[1][0];
-$durum = $veri_durum[2][0];
+	$derece= $veri_derece[1][0];
+	$durum = $veri_durum[2][0];
+
+	$bot = new bot();
+	$bot->veritabani_ekle($il,$derece,$durum);
+
+}
+
+//$il = "Istanbul";
+
+
+
+
+
 //var_dump($veri_derece); die;
 
 
@@ -29,16 +48,12 @@ echo "</pre>";
 */
 
 /*
-echo $veri_derece[1][0];
-echo $veri_durum[2][0];
-
+echo $derece." ";
+echo $durum;
 */
 
 
 
-
-$bot = new bot();
-$bot->veritabani_ekle($il,$veri_derece[0][0],$veri_durum[2][0]);
 
 
 ?>
