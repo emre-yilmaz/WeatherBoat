0cbe3355785ead6ebd7d74dee79a482f09c30630
Cenk Gün <cenk.gn@gmail.com>
2015-05-07 15:49:12 +0300

    connection script
    bağlantı düzenlemeleri yapıldı

--- Connection.php
+++ Connection.php
@@ -10,16 +10,17 @@ class Connection {
 
     public static  function create(){
 
-        $dsn = 'mysql:dbname=havatahmin_bot;host=LOCALHOST';
+        $dsn = 'mysql:dbname=havatahmin_bot;host=LOCALHOST;charset=utf8';
         $user = 'root';
         $password = '';
 
         try
         {
-            return new PDO($dsn, $user, $password,[PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
+            return new PDO($dsn, $user, $password,[PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,PDO::MYSQL_ATTR_INIT_COMMAND => "SET character_set_results = 'utf8', character_set_client = 'utf8', character_set_connection = 'utf8', character_set_database = 'utf8', character_set_server = 'utf8'"]);
         } catch (PDOException $e) {
             echo 'Bağlantı Sağlanılamadı.: ' . $e->getMessage();
         }
+
 
     }
 
