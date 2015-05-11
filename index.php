bfcd1ef83e9f6f25d7d18d4fe7e6d689456deb57
Cenk Gün <cenk.gn@gmail.com>
2015-05-11 11:54:54 +0300

    15 Günlük Hava Raporu
    15 days Weather

--- index.php
+++ index.php
@@ -7,24 +7,16 @@ $parcala= '@<td style="text-align: center;">(.*?)</td>@si'; // 15 günlük hava 
 $botara=file_get_contents($link);
 preg_match_all($parcala,$botara,$ifade);
 
-
 $parcala2 = '@<td style="text-align: left;">(.*?)</td>@si';
-
 preg_match_all($parcala2,$botara,$ifade2);
-
-
-
 
 $parcala3= '@<td>(.*?)</td>@si';
 preg_match_all($parcala3,$botara,$ifade3);
-
-
 
 $sayac=0;
 for($i=0;$i<30;$i++){
 
 	if($i%2==0){
-
 		$sicaklik[0][$sayac]=$ifade[0][$i];
 		$tarih[$sayac]=$ifade3[0][$i];
 		$sayac++;
@@ -32,11 +24,9 @@ for($i=0;$i<30;$i++){
 	else{
 		$sicaklik[1][$sayac]=$ifade[1][$i];
 	}
-
 }
 
 for($i=0; $i<15; $i++){
-
 	echo $tarih[$i]."<br/>"."Gündüz: ".$sicaklik[0][$i]." "."Durum:  ".$ifade2[0][$i]."<br/>"."Gece: ".$sicaklik[1][$i+1]."<br/>"."<br/>";
 }
 
